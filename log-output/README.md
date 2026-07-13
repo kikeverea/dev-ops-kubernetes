5b89a8b8e5102ff7f558765422815eff887669c0
Enrique Verea <kikeverea@gmail.com>
2026-07-13 23:07:55 +0200

    Exercise 1.7 done

--- log-output/README.md
+++ log-output/README.md
@@ -1,1 +1,2 @@
-Run with `kubectl apply -f manifests/deployment.yml `
+Run with `kubectl apply -f manifests/`
+If using k3d proxy a port to the load balancer htpp. Eg: `k3d cluster create kuber-mooc --port 8082:30080@agent:0 -p 8081:80@loadbalancer --agents 2`
--- log-output/index.ts
+++ log-output/index.ts
@@ -1,11 +1,18 @@
 import crypto from 'crypto'
+import express from 'express'
 
 const str = crypto.randomBytes(20).toString('hex')
+const formatString = (str: string) => `${new Date().toISOString()}: ${str}`
+const printStr = (str: string) => console.log(formatString(str))
 
-const printStr = (str: string) =>
-  console.log(`${new Date().toISOString()}:`, str)
+const app = express()
+const port = process.env.port || 3000
 
 printStr(str)
-
 setInterval(() => printStr(str), 5000)
 
+app.get('/', (_req, res) =>
+  res.send(formatString(str))
+)
+
+app.listen(port, () => console.log(`Server started in port ${port}`))
